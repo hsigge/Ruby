a2149635ba25bbfac30541fc7cce23e91e598df3
HSigge <hanna.sigge@gmail.com>
2018-08-11 14:07:13 +0200

    added eat

--- dog.rb
+++ dog.rb
@@ -1,20 +1,28 @@
 class Dog
-    attr_accessor :breed, :name
+    attr_accessor :breed, :name, :sex
 
-    def initialize(breed, name)
+    def initialize(breed, name, sex)
         @breed = breed
         @name = name
+        @sex = sex
     end
     
     def wag_tail
-        puts "Tail wagging"
+        puts "wags tail"
     end
 
     def bark
         puts "barks"
     end
+
+    def eat
+        puts "eats kibble"
+    end
+
 end
 
-dog = Dog.new ("softis", "Trixie")
+dog = Dog.new("softis", "Trixie", "bitch")
 
+dog.wag_tail
 dog.bark
+dog.eat
