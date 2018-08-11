ded9683459efa246fdc37acf3d2894187454563e
HSigge <hanna.sigge@gmail.com>
2018-08-11 12:12:09 +0200

    dog.rb file

---
+++ dog.rb
@@ -0,0 +1,12 @@
+class Dog
+    attr_accessor :breed, :name
+
+    def initialize(breed, name)
+        @breed = breed
+        @windows = windows
+    end
+    
+    def wag_tail
+        puts "Tail wagging"
+    end
+end
