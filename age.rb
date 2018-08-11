9a29126006b4eeefed76d58fbaa8dab004bcdbd2
HSigge <hanna.sigge@gmail.com>
2018-08-11 17:41:35 +0200

    Ruby variables age exercise

---
+++ age.rb
@@ -0,0 +1,20 @@
+puts "What is your age?"
+age = gets.chomp.to_i
+
+puts "You are #{age} years old."
+
+print "In 10 years, you will be " 
+print age + 10 
+puts " years old. Practically a baby!"
+
+print "In 20 years, you will be " 
+print age + 20 
+puts " years old."
+
+print "In 30 years, you will be " 
+print age + 30 
+puts " years old."
+
+print "In 40 years, you will be " 
+print age + 40 
+puts " years old. That's ancient!"
