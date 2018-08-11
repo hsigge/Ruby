cde065395455cfc1299fabbef8a4650c6308a8bc
HSigge <hanna.sigge@gmail.com>
2018-08-11 17:53:48 +0200

    added last name

--- name.rb
+++ name.rb
@@ -1,6 +1,9 @@
-puts "What is your name?"
-name = gets.chop
-puts "Hi #{name}! I'm Ruby."
-10.times do
-    puts "#{name}"
+puts "What is your first name?"
+first_name = gets.chomp
+puts "Now tell me your last name."
+last_name = gets.chomp
+puts "So your full name is #{first_name} #{last_name}. Pleased to meet you."
+3.times do
+    puts "#{first_name}!"
 end
+puts "#{first_name} Banana!"
