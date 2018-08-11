b216c6518fe7f94a5766e13d05f1dff47357564a
HSigge <hanna.sigge@gmail.com>
2018-08-11 13:44:06 +0200

    added barks

--- dog.rb
+++ dog.rb
@@ -3,10 +3,18 @@ class Dog
 
     def initialize(breed, name)
         @breed = breed
-        @windows = windows
+        @name = name
     end
     
     def wag_tail
         puts "Tail wagging"
     end
+
+    def bark
+        puts "barks"
+    end
 end
+
+dog = Dog.new ("softis", "Trixie")
+
+dog.bark
